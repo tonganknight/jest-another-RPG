0ee781a7b07a38a4d3088a3bb8249afbd00c509a
Malachi <malachialusa@gmail.com>
2021-01-18 17:17:09 -0700

    added basic potion function

---
+++ _tests_/Potion.test.js
@@ -0,0 +1,25 @@
+
+
+function Potion(name) {
+    this.types = ['strength', 'agility', 'health'];
+    this.name = name || this.types[Math.floor(Math.random() * this.types.length)];
+  
+    if (this.name === 'health') {
+      this.value = Math.floor(Math.random() * 10 + 30);
+    } else {
+      this.value = Math.floor(Math.random() * 5 + 7);
+    }
+  }
+  
+  module.exports = Potion;
+  
+  module.exports = Potion;
+
+
+  test('creates a random potion object', () => {
+    const potion = new Potion();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
