a5d3092cdcdbe6fe4d72c201be3225c5863e7028
Malachi <malachialusa@gmail.com>
2021-01-19 10:19:54 -0700

    I updated the player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,3 +32,45 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
+});
+
+Player.prototype.getHealth = function() {
+  return `${this.name}'s health is now ${this.health}!`;
+};
+
+Player.prototype.isAlive = function() {
+  if (this.health === 0) {
+    return false;
+  }
+  return true;
+};
+
+Player.prototype.reduceHealth = function(health) {
+  this.health -= health;
+
+  if (this.health < 0) {
+    this.health = 0;
+  }
+};
